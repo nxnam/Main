694d78df2ced66c035fac1fd1917e3078b733ec5
Nam <nxnamvn@gmail.com>
2022-10-19 03:11:20 +0700

    update code

--- Sources/Main/MainViewController.swift
+++ Sources/Main/MainViewController.swift
@@ -55,6 +55,23 @@ open class MainViewController: UITabBarController, MainPresentable, MainViewCont
 
 }
 
+extension UIViewController {
+    public func add(asChildViewController viewController: UIViewController, to parentView: UIView) {
+        // Add Child View Controller
+        addChild(viewController)
+
+        // Add Child View as Subview
+        parentView.addSubview(viewController.view)
+
+        // Configure Child View
+        viewController.view.frame = parentView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
+    }
+}
+
 enum TabBarTag: Int {
     case home = 0
     case following = 1
