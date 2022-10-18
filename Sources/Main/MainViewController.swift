ad3e219adf1001d2b9f1b1e1ff2ea27ce65265b9
Nam <nxnamvn@gmail.com>
2022-10-19 03:00:14 +0700

    update code

--- Sources/Main/MainViewController.swift
+++ Sources/Main/MainViewController.swift
@@ -9,6 +9,9 @@
 import RIBs
 import RxSwift
 import UIKit
+import Home
+import Following
+import Personal
 
 protocol MainPresentableListener: AnyObject {
     // TODO: Declare properties and methods that the view controller can invoke to perform
@@ -16,7 +19,68 @@ protocol MainPresentableListener: AnyObject {
     // interactor class.
 }
 
-final class MainViewController: UIViewController, MainPresentable, MainViewControllable {
+open class MainViewController: UITabBarController, MainPresentable, MainViewControllable {
 
     weak var listener: MainPresentableListener?
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.viewControllers = initChildsVC()
+    }
+    
+    private func initChildsVC() -> [UINavigationController] {
+        var results = [UINavigationController]()
+
+        // Home
+        let homeVC = HomeViewController()
+        let homeNavVC = UINavigationController(rootViewController: homeVC)
+        homeNavVC.tabBarItem = TabBarTag.home.toUITabBarItem
+        results.append(homeNavVC)
+
+        // Following
+        let followingVC = FollowingViewController()
+        let followingNavVC = UINavigationController(rootViewController: followingVC)
+        followingNavVC.tabBarItem = TabBarTag.following.toUITabBarItem
+        results.append(followingNavVC)
+
+        // Personal
+        let personalVC = PersonalViewController()
+        let personalNavVC = UINavigationController(rootViewController: personalVC)
+        personalNavVC.tabBarItem = TabBarTag.personal.toUITabBarItem
+        results.append(personalNavVC)
+
+        return results
+    }
+
 }
+
+enum TabBarTag: Int {
+    case home = 0
+    case following = 1
+    case personal = 2
+
+    var toUITabBarItem: UITabBarItem {
+        switch self {
+        case .home:
+            let uiTabBar = UITabBarItem(title: "Home", image: UIImage(named: "ic_home_tab_bar_normal"), tag: TabBarTag.home.rawValue)
+            return uiTabBar
+        case .following:
+            let uiTabBar = UITabBarItem(title: "Following", image: UIImage(named: "ic_following_tab_bar_normal"), tag: TabBarTag.following.rawValue)
+            return uiTabBar
+        case .personal:
+            let uiTabBar = UITabBarItem(title: "Personal", image: UIImage(named: "ic_personal_tab_bar_normal"), tag: TabBarTag.personal.rawValue)
+            return uiTabBar
+        }
+    }
+}
+
+enum MainModel {
+
+    struct TabBarItem {
+        let title: String
+        let image: UIImage
+        let tag: TabBarTag
+    }
+}
+
